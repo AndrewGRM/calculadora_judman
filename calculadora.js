b229d901c1eecf77dfcfaa850e70bc58c388d6ff
AndrewGRM <andrew.grm15@gmail.com>
2025-04-20 21:00:30 -0300

    Colocando tudo em variaveis na função calcular_valor_cedivel

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,5 @@
 const data = new Date();
-const dataInicial = '01/01/2025';
+const dataInicial = '01/02/2025';
 
 const mesAnterior = new Date(data);
 mesAnterior.setMonth(data.getMonth() - 1);
@@ -11,7 +11,7 @@ console.log(dataFinal);
 
 const url = `https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json&dataInicial=${dataInicial}&dataFinal=${dataFinal}`;
 
-async function obterSomaSelic(url) {
+async function obter_e_Somar_Selic(url) {
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -24,6 +24,7 @@ async function obterSomaSelic(url) {
             return soma + valor;
         }, 0);
 
+        console.log(`Porcentagem Selic ${somaSelic}`);
         return somaSelic;
 
     } catch (error) {
@@ -33,17 +34,17 @@ async function obterSomaSelic(url) {
 }
 
 async function calcular_valor_cedivel(valor_bruto, percentual_honorario, rra=0.03) {
-    const somaSelic = await obterSomaSelic(url);
+    const somaSelic = await obter_e_Somar_Selic(url);
     // Faz o calculo da Selic e soma com o valor bruto
     var selic_valor = valor_bruto * (somaSelic / 100)
     var valor_liquido = valor_bruto + selic_valor
-
+    var valor_honorario = (valor_liquido * (percentual_honorario / 100))
     // Desconta o valor do RRA e o percentual do honorário
     var valor_rra = valor_liquido * rra
-    var valor_recebido = valor_liquido - (valor_liquido * (percentual_honorario / 100)) - valor_rra
+    var valor_recebido = valor_liquido - valor_honorario - valor_rra
 
     // Retorna o valor final arredondado
-    return valor_recebido
+    return somaSelic, valor_liquido, valor_rra, selic_valor, valor_honorario, valor_recebido
 }
 
 calcular_valor_cedivel(200000, 30).then(valor => {
